b8dbdce9892c58b533013a095a41ae69c8ae7390
Kabe <kabweynew@gmail.com>
2026-09-18 11:28:02 +0300

    Update

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'agash-v1';
+const PRECACHE = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })
+      .catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
